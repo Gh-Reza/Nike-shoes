5d7e6f7b80171f3f4b9f6e3f60815ae3e153d91f
reza <ghrezam97@gmail.com>
2023-12-21 13:16:40 +0430

    Add mobile menu

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,15 +1,35 @@
 import NikeLogo from "../assets/nike-logo.svg?react";
 import { RxHamburgerMenu } from "react-icons/rx";
 
+const ROUTES = ["Home", "About", "Services", "Pricing", "Contact"];
 const Nav = () => {
   return (
-    <nav className="flex items-center justify-between">
+    <nav className="flex flex-wrap items-center justify-between">
       {/* Nike logo */}
       <NikeLogo className="w-20 h-20" />
+
       {/* Hamburger menu */}
       <button className="p-1 rounded-sm hover:bg-gray-100 focus:ring-2 focus:ring-gray-200">
         <RxHamburgerMenu size={25} />
       </button>
+
+      {/* Menu list */}
+      <div className="w-full">
+        <ul className="p-4 text-lg rounded bg-gray-50">
+          {ROUTES.map((route, i) => (
+            <li
+              className={`px-3 py-2 rounded ${
+                i === 0
+                  ? "first:bg-blue-500 first:text-white"
+                  : "hover:bg-gray-100"
+              } `}
+              key={route}
+            >
+              {route}
+            </li>
+          ))}
+        </ul>
+      </div>
     </nav>
   );
 };
