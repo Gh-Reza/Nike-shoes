fe9fc727c72cb686d88b892638f463ef61adfbcc
reza <ghrezam97@gmail.com>
2023-12-21 14:04:20 +0430

    Handle hamburger menu click

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,22 +1,27 @@
 import { FaShoppingBag } from "react-icons/fa";
 import NikeLogo from "../assets/nike-logo.svg?react";
 import { RxHamburgerMenu } from "react-icons/rx";
+import { useState } from "react";
 
 const ROUTES = ["Home", "About", "Services", "Pricing", "Contact"];
 const Nav = () => {
+  const [isMenuShown, setMenuShown] = useState(false);
   return (
     <nav className="flex flex-wrap items-center justify-between">
       {/* Nike logo */}
       <NikeLogo className="w-20 h-20" />
 
       {/* Hamburger menu */}
-      <button className="p-1 rounded-sm lg:hidden hover:bg-gray-100 focus:ring-2 focus:ring-gray-200">
+      <button
+        onClick={() => setMenuShown(!isMenuShown)}
+        className="p-1 rounded-sm lg:hidden hover:bg-gray-100 focus:ring-2 focus:ring-gray-200"
+      >
         <RxHamburgerMenu size={25} />
       </button>
 
       {/* Menu list */}
-      <div className="w-full lg:w-auto">
-        <ul className="p-4 text-lg rounded lg:flex bg-gray-50 lg:bg-transparent">
+      <div className={`w-full lg:w-auto lg:block ${!isMenuShown && "hidden"}`}>
+        <ul className="p-4 text-lg rounded lg:space-x-6 lg:flex bg-gray-50 lg:bg-transparent">
           {ROUTES.map((route, i) => (
             <li
               className={`px-3 py-2 rounded cursor-pointer ${
